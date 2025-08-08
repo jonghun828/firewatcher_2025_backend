871132e8d2672486038a4eafffa2f177d3573eef
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-08-08 10:45:21 +0900

    chore : first commit

---
+++ src/main/java/com/metaverse/wildfirewatcher_2025/WildfireWatcher2025Application.java
@@ -0,0 +1,13 @@
+package com.metaverse.wildfirewatcher_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildfireWatcher2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WildfireWatcher2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/metaverse/wildfirewatcher_2025/WildfireWatcher2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.wildfirewatcher_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildfireWatcher2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
