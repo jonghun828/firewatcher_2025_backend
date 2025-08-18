0686808c21a5932f0ca1d7771233e467eeb986bc
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-08-18 11:12:13 +0900

    feat : NoticeRequestDto 추가

---
+++ src/main/java/com/metaverse/wildfirewatcher_2025/notice/dto/NoticeRequestDto.java
@@ -0,0 +1,9 @@
+package com.metaverse.wildfirewatcher_2025.notice.dto;
+
+import lombok.Getter;
+
+@Getter
+public class NoticeRequestDto {
+    private String title;
+    private String content;
+}
