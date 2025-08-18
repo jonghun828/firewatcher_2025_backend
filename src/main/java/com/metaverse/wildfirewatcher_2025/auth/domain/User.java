4a06b5adaf0220cd61c8028e5aec09fb89ec2063
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-08-18 14:27:30 +0900

    feat : auth/domain/User 추가

---
+++ src/main/java/com/metaverse/wildfirewatcher_2025/auth/domain/User.java
@@ -0,0 +1,43 @@
+package com.metaverse.wildfirewatcher_2025.auth.domain;
+
+import com.metaverse.wildfirewatcher_2025.common.domain.TimeStamped;
+import com.metaverse.wildfirewatcher_2025.notice.domain.Notice;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="User")
+public class User extends TimeStamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phone_number;
+
+    @Column(nullable = false)
+    private int role_id;
+
+    @Column(nullable = false)
+    private int zone_id;
+
+    @OneToMany(mappedBy = "user")
+    List<Notice> notices =  new ArrayList<>();
+}
