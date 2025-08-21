b180c407e6f00bcf16fa64e938fd67d228bbb69b
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-08-21 17:51:11 +0900

    delete : NoticeService CRUD 기능 추가

--- src/main/java/com/metaverse/wildfirewatcher_2025/notice/service/NoticeService.java
+++ src/main/java/com/metaverse/wildfirewatcher_2025/notice/service/NoticeService.java
@@ -1,20 +1,55 @@
 package com.metaverse.wildfirewatcher_2025.notice.service;
 
 import com.metaverse.wildfirewatcher_2025.notice.domain.Notice;
+import com.metaverse.wildfirewatcher_2025.notice.dto.NoticeRequestDto;
 import com.metaverse.wildfirewatcher_2025.notice.dto.NoticeResponseDto;
+import com.metaverse.wildfirewatcher_2025.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
 private final NoticeRepository noticeRepository;
 
-//공지사항 전체 조회.
+    //공지사항 전체 조회.
+    @Transactional(readOnly = true)
     public Page<NoticeResponseDto> getNotices(Pageable pageable) {
         Page<Notice> noticePage = noticeRepository.findAll(pageable);
         return noticePage.map(NoticeResponseDto::new);
+    }
+
+    //공지사항 생성.
+    @Transactional
+    public Page<NoticeResponseDto> createNotice(NoticeRequestDto noticeRequestDto) {
+        Notice notice = new Notice(noticeRequestDto);
+        Notice savedNotice = noticeRepository.save(notice);
+        NoticeResponseDto noticeResponseDto = new NoticeResponseDto(savedNotice);
+        return (Page<NoticeResponseDto>) noticeResponseDto;
+    }
+
+    //공지사항 수정.
+    @Transactional
+    public Long updateNotice(Long id, NoticeRequestDto noticeRequestDto) {
+        Notice notice = findNotice(id);
+        notice.update(noticeRequestDto);
+        return id;
+    }
+
+    //공지사항 삭제.
+    @Transactional
+    public Long deleteNotice(Long id) {
+        Notice notice = findNotice(id);
+        noticeRepository.delete(notice);
+        return id;
+    }
+
+    private Notice findNotice(Long id) {
+        return noticeRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("해당 게시물은 존재하지 않습니다.")
+        );
     }
 }
