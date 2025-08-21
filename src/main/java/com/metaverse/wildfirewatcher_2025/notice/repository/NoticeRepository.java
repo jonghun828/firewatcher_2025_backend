febcd1652f50155d34ce1ad6a414a0c45a00058c
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-08-21 17:49:06 +0900

    feat : NoticeRepository findAll 추가

--- src/main/java/com/metaverse/wildfirewatcher_2025/notice/repository/NoticeRepository.java
+++ src/main/java/com/metaverse/wildfirewatcher_2025/notice/repository/NoticeRepository.java
@@ -1,10 +1,11 @@
 package com.metaverse.wildfirewatcher_2025.notice.repository;
 import com.metaverse.wildfirewatcher_2025.notice.domain.Notice;
-import com.metaverse.wildfirewatcher_2025.notice.dto.NoticeResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
-
+    Page<Notice> findAll(Pageable pageable);
 }
