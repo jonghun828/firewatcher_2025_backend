d0364a1c10a2dceaf89d3cc40c6d262d31c719b0
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-08-22 16:07:46 +0900

    feat : 생성된 순서대로 내리막순으로 배열 명령어 추가

--- src/main/java/com/metaverse/wildfirewatcher_2025/notice/repository/NoticeRepository.java
+++ src/main/java/com/metaverse/wildfirewatcher_2025/notice/repository/NoticeRepository.java
@@ -1,11 +1,15 @@
 package com.metaverse.wildfirewatcher_2025.notice.repository;
 import com.metaverse.wildfirewatcher_2025.notice.domain.Notice;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    List<Notice> findAllByOrderByCreatedAtDesc();
     Page<Notice> findAll(Pageable pageable);
 }
