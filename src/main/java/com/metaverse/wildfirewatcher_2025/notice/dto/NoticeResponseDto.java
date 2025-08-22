02f80b3b1a57ed08e4f77d6cd2e2fa723ba092ed
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-08-22 16:07:19 +0900

    feat : important 속성 추가, Date -> LocalDateTime으로 타입 변경

--- src/main/java/com/metaverse/wildfirewatcher_2025/notice/dto/NoticeResponseDto.java
+++ src/main/java/com/metaverse/wildfirewatcher_2025/notice/dto/NoticeResponseDto.java
@@ -4,30 +4,34 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.metaverse.wildfirewatcher_2025.comment.dto.CommentResponseDto;
 import com.metaverse.wildfirewatcher_2025.common.domain.TimeStamped;
 import com.metaverse.wildfirewatcher_2025.notice.domain.Notice;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class NoticeResponseDto extends TimeStamped {
     private Long id;
     private String title;
     private String content;
+    private boolean important;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date createAt;
+    private LocalDateTime createdAt;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date modifiedAt;
-
-    private List<CommentResponseDto> comments;
+    private LocalDateTime modifiedAt;
 
 public NoticeResponseDto(Notice notice) {
     this.id = notice.getId();
     this.title = notice.getTitle();
     this.content = notice.getContent();
-    this.createAt =notice.getCreatedAt();
+    this.important = notice.isImportant();
+    this.createdAt = notice.getCreatedAt();
     this.modifiedAt = notice.getModifiedAt();
 
 
