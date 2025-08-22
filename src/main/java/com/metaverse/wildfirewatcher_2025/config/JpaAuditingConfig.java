a567b73ddb4cc01d0bf0880201fbead8cf8c5a85
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-08-22 16:06:04 +0900

    fix : 임시로 주석 처리

--- src/main/java/com/metaverse/wildfirewatcher_2025/config/JpaAuditingConfig.java
+++ src/main/java/com/metaverse/wildfirewatcher_2025/config/JpaAuditingConfig.java
@@ -2,16 +2,13 @@ package com.metaverse.wildfirewatcher_2025.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class JpaAuditingConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
